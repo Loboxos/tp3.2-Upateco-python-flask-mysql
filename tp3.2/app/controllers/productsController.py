68f45c4c26b2dc40021f38a231eb96d60ff193b7
Loboxos <vcristian528@gmail.com>
2023-08-30 20:53:50 -0300

    get_all_products hecho

--- tp3.2/app/controllers/productsController.py
+++ tp3.2/app/controllers/productsController.py
@@ -40,3 +40,26 @@ class productController:
         else:
             return jsonify({"error": "Producto no encontrado"}), 404
     
+    @classmethod
+    def get_products(self):
+        products_list = products.get_all_products()
+        response = []
+        for product in products_list:
+            #print(product)
+            product_data = {
+            "brand": {
+                "brand_id": product["brand"]["brand_id"],
+                "brand_name": product["brand"]["brand_name"]
+            },
+            "category": {
+                "category_id": product["category"]["category_id"],
+                "category_name": product["category"]["category_name"]
+            },
+            "list_price": str(product["list_price"]),
+            "model_year": product["model_year"],
+            "product_id": product["product_id"],
+            "product_name": product["product_name"]
+        }
+            response.append(product_data)
+
+        return jsonify(response), 200
--- tp3.2/app/models/products.py
+++ tp3.2/app/models/products.py
@@ -55,3 +55,35 @@ class products:
         connection.commit()
         cursor.close()
         return True
+
+    @classmethod
+    def get_all_products(cls):
+                query = '''
+                SELECT
+                    p.product_id,
+                    p.product_name,
+                    b.brand_id,
+                    b.brand_name,
+                    c.category_id,
+                    c.category_name,
+                    p.model_year,
+                    p.list_price
+                FROM
+                    products p
+                    JOIN brands b ON p.brand_id = b.brand_id
+                    JOIN categories c ON p.category_id = c.category_id
+                '''
+                results = DatabaseConnection.fetch_all(query)
+                products_list = []
+
+                for result in results:
+                    products_list.append({
+                        "product_id": result[0],
+                        "product_name": result[1],
+                        "brand": {"brand_id": result[2], "brand_name": result[3]},
+                        "category": {"category_id": result[4], "category_name": result[5]},
+                        "model_year": result[6],
+                        "list_price": result[7]
+                    })
+
+                return products_list
